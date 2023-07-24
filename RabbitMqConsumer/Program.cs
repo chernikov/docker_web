d0fa361e1e2641679e54e754f3a276f38bcca422
Andriy Chernikov <andriy.chernikov@emburse.com>
2023-07-24 12:20:59 +0300

    Add RabbitMQ

---
+++ RabbitMqConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿// Create a connection factory
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost", // Replace with your RabbitMQ server's hostname
+    UserName = "guest",     // Replace with your RabbitMQ username
+    Password = "guest"      // Replace with your RabbitMQ password
+    
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    // Declare a queue
+    channel.QueueDeclare(queue: "my_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    // Create a consumer and set up event handling for received messages
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine("Message received: {0}", message);
+    };
+
+    // Start consuming messages from the queue
+    channel.BasicConsume(queue: "my_queue", autoAck: true, consumer: consumer);
+
+    Console.WriteLine("Waiting for messages. Press any key to exit.");
+    Console.ReadLine();
+}
---
+++ RabbitMqSender/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost", // Replace with your RabbitMQ server's hostname
+    UserName = "guest",     // Replace with your RabbitMQ username
+    Password = "guest"      // Replace with your RabbitMQ password
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    // Declare a queue
+    channel.QueueDeclare(queue: "my_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    // Create a message
+    var rand = new Random();
+
+    
+    var key = ' ';
+    while (key != 'q')
+    {
+        var next = rand.Next(100);
+        string message = $"Hello, {next}";
+        var body = Encoding.UTF8.GetBytes(message);
+
+
+        channel.BasicPublish(exchange: "", routingKey: "my_queue", basicProperties: null, body: body);
+        Console.WriteLine("Message sent: {0}", message);
+        Console.WriteLine("Press `q` to exit.");
+        key = Console.ReadKey().KeyChar;
+    }
+    // Publish the message to the queue
+    
+}
+
+
+Console.ReadLine();
